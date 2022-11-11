2d7438a44a32e0c6f4cf17070a7a5c80fe4499f6
caseynv <nwandukelechi@gmail.com>
2022-11-11 23:28:29 +0100

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xs: "200px",
+      ...defaultTheme.screens,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
